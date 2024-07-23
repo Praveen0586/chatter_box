c1aba311a099e3726101c20d29ab6e6e97a240a4
Praveen0586 <praveensuresh1605@gmail.com>
2024-07-24 03:48:12 +0530

    login screen with validation and local stroring

--- lib/screens/authentication.dart
+++ lib/screens/authentication.dart
@@ -9,6 +9,21 @@ class AuthenticationScreen extends StatefulWidget {
 }
 
 class _AuthenticationScreenState extends State<AuthenticationScreen> {
+  String _selectedEmail = '';
+  String _selectedPassword = '';
+  bool isLogin = true;
+  final _formkey = GlobalKey<FormState>();
+
+  void _loginorsignup() {
+    final isValid = _formkey.currentState!.validate();
+
+    if (isValid) {
+      _formkey.currentState!.save();
+      print(_selectedEmail);
+      print(_selectedPassword);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,42 +57,94 @@ class _AuthenticationScreenState extends State<AuthenticationScreen> {
                 Padding(
                   padding: const EdgeInsets.all(12),
                   child: Form(
+                      key: _formkey,
                       child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      TextFormField(
-                        textCapitalization: TextCapitalization.none,
-                        keyboardType: TextInputType.emailAddress,
-                        autocorrect: false,
-                        decoration: InputDecoration(
-                            label: Text(
-                          'Email Addreass ',
-                          style: Theme.of(context)
-                              .textTheme
-                              .titleMedium!
-                              .copyWith(
-                                  color:
-                                      Theme.of(context).colorScheme.onPrimary),
-                        )),
-                      ),
-                      TextFormField(
-                        obscureText: true,
-                        textCapitalization: TextCapitalization.none,
-                        keyboardType: TextInputType.emailAddress,
-                        autocorrect: false,
-                        decoration: InputDecoration(
-                            label: Text(
-                          'Password',
-                          style: Theme.of(context)
-                              .textTheme
-                              .titleMedium!
-                              .copyWith(
-                                  color:
-                                      Theme.of(context).colorScheme.onPrimary),
-                        )),
-                      )
-                    ],
-                  )),
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          TextFormField(
+                            textCapitalization: TextCapitalization.none,
+                            keyboardType: TextInputType.emailAddress,
+                            autocorrect: false,
+                            decoration: InputDecoration(
+                                label: Text(
+                              'Email Address ',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleMedium!
+                                  .copyWith(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .onPrimary),
+                            )),
+                            validator: (value) {
+                              if (value == null ||
+                                  value.trim().isEmpty ||
+                                  !value.contains('@')) {
+                                return 'Enter a valid Email Address';
+                              }
+                              return null;
+                            },
+                            onSaved: (newValue) {
+                              _selectedEmail = newValue!;
+                            },
+                          ),
+                          TextFormField(
+                            obscureText: true,
+                            textCapitalization: TextCapitalization.none,
+                            keyboardType: TextInputType.emailAddress,
+                            autocorrect: false,
+                            decoration: InputDecoration(
+                                label: Text(
+                              'Password',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleMedium!
+                                  .copyWith(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .onPrimary),
+                            )),
+                            validator: (value) {
+                              if (value == null ||
+                                  value.trim().isEmpty ||
+                                  value.trim().length < 6) {
+                                return 'Password must contain six Charectors';
+                              }
+                              return null;
+                            },
+                            onSaved: (newValue) {
+                              _selectedPassword = newValue!;
+                            },
+                          ),
+                          const SizedBox(
+                            height: 50,
+                          ),
+                          ElevatedButton.icon(
+                              onPressed: _loginorsignup,
+                              icon: Icon(isLogin
+                                  ? Icons.lightbulb_outline
+                                  : Icons.bubble_chart_outlined),
+                              label: Text(isLogin ? 'LogIn' : 'SignIn')),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          TextButton.icon(
+                              onPressed: () {
+                                setState(() {
+                                  isLogin = !isLogin;
+                                });
+                              },
+                              icon: Icon(isLogin
+                                  ? Icons.bubble_chart_outlined
+                                  : Icons.lightbulb_outline),
+                              label: Text(isLogin
+                                  ? 'Create a New Account'
+                                  : 'Login with your Account'),
+                              style: TextButton.styleFrom(
+                                  foregroundColor:
+                                      Theme.of(context).colorScheme.onPrimary))
+                        ],
+                      )),
                 )
               ],
             )),
